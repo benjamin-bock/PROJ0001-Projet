00e1b4d0b2f6f4362c1f88fd5ce8e202439da3b9
Benjamin Bock <benjaminbock05@gmail.com>
2025-02-24 14:39:49 +0100

    feat: add secante function

--- RechercheRacine.py
+++ RechercheRacine.py
@@ -28,10 +28,90 @@ def bissection(f, x0, x1, tol):
             return [x, 0]
 
         # Mettre à jour l'intervalle
-        if f_x * f_a < 0:
+        elif f_x * f_a < 0:
             b = x
         else:
             a = x
             f_a = f_x  # Réutiliser f_x pour la prochaine itération
 
     return [(a + b) / 2.0, 0]  # statut = 0 -> convergence réussie
+
+def secante(f, x0, x1, tol):
+    
+    # Vérifier les hypothèses de la méthode de la bissection
+    f_x0 = f(x0)
+    f_x1 = f(x1)
+    if f_x0 * f_x1 >= 0.0:
+        print("Erreur : f(x0) et f(x1) doivent avoir des signes opposés.")
+        return [None, 1]  # statut = 1 -> hypothèses non satisfaites
+
+    iteration = 0
+    max_iter = 100000  # Nb max d'itérations pour éviter une boucle infinie
+    f_x2 = 100 # On initialise un grand nombre pour ne pas sortir de la boucle
+    
+    while iteration < max_iter :
+              
+        iteration += 1 
+        
+        # Éviter la division par zéro
+        if f_x1 - f_x0 == 0.0 :
+            print("Erreur : Division par zéro")
+            return [None, 1] # statut = 1 -> Processus interrompu
+
+        # Calculer la nouvelle itérée
+        x2 = x1 - f_x1 * (x1 - x0) / (f_x1 - f_x0)
+
+        # On appelle une seule fois f
+        f_x2 = f(x2)
+        
+        # Critère d'arrêt
+        if abs(f_x2) < tol :
+            print("La méthode a convergé")
+            return [x2, 0]
+        
+        # Mise à jour des paramètres
+        
+        # y devient x0
+        if f_x1 * f_x2 < 0.0 :
+            x0 = x1
+            f_x0 = f_x1
+            
+        # x2 devient x1
+        
+        x1 = x2
+        f_x1 = f_x2
+        
+
+    print("Erreur : La méthode n'a pas convergé")
+    return [None, -1] # statut = -1 -> non-convergence
+            
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- test_bissection.py
+++ test_bissection.py
@@ -5,14 +5,18 @@ Created on Mon Feb 24 11:09:09 2025
 @author: b3nja
 """
 import numpy as np
-from RechercheRacine import bissection
+from RechercheRacine import bissection, secante
 
 
 # Définir une fonction
 def f(x):
-    return x**5 -x**4 + 4*x**3 -18*x**2 +(np.pi)*x - 15*(np.log(3*x))
+    return x**3 - x - 2
 
 # Utiliser la méthode de bissection
-resultat_bissection = bissection(f, 100, 1, 1e-15)
+resultat_bissection = bissection(f, -50, 100, 1e-15)
 print("Bissection :", resultat_bissection)
+
+# Utiliser la méthode de sécante
+resultat_secante = secante(f, -50, 100, 1e-6)
+print("Sécante :", resultat_secante)
 
