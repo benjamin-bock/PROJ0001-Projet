46f911a0f85f9e0dcbda3bb333d78a7af087faa7
YAZ357 <yazansaloum373@gmail.com>
2025-02-27 14:09:28 +0100

    Add files via upload

---
+++ constante.py
@@ -0,0 +1,14 @@
+# Capacitances thermiques (en kJ/m²K)
+C_cc = 50
+C_c1 = 50
+C_c2 = 10
+C_room = 12
+C_w = 30
+
+# Résistances thermiques (en m²K/W)
+R_cc_c1 = 0.05
+R_x = 0.025
+R_c2_cc = 0.02
+R_r_s = 0.1
+R_s_c2 = 0.183
+R_w = 0.15
